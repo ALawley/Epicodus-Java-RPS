322e0acf44b0e31112bbbf8767cd3547733b932f
Anna Lawley and Megan Fayer <student@epicodus.com>
2016-02-10 09:37:42 -0800

    link pages properly and complete functionality.

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -1,14 +1,47 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
 public class RockPaperScissors {
   public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
+
+   get("/", (request, response) -> {
+     Map<String, Object> model = new HashMap<String, Object>();
+     model.put("template", "templates/home.vtl");
+     return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
+
+   get("/TwoPlayers", (request, response) -> {
+     Map<String, Object> model = new HashMap<String, Object>();
+     model.put("template", "templates/TwoPlayers.vtl");
+     return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
+
+   get("/checkWinner", (request, response) -> {
+     Map<String, Object> model = new HashMap<String, Object>();
+     model.put("template", "templates/checkWinner.vtl");
+
+     String player1 = request.queryParams("player1");
+     String player2 = request.queryParams("player2");
+     String result = whoWins(player1, player2);
+
+     model.put("result", result);
+     return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
+
+
 
   }
   public static Boolean checkWinner(String input1, String input2) {
     Boolean result = false;
-    if (input1 == "Rock" && input2 == "Scissors"){
+    if (input1.equals("Rock") && input2.equals("Scissors")){
       result = true;
-    } else if (input1 == "Scissors" && input2 == "Paper"){
+    } else if (input1.equals("Scissors") && input2.equals("Paper")){
       result = true;
-    } else if (input1 == "Paper" && input2 == "Rock"){
+    } else if (input1.equals("Paper") && input2.equals("Rock")){
       result = true;
     }
     return result;
